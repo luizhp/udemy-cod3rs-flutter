75e495cde062cd9d2aaf4a0ad46d7ee558d353fc
luizhp <luizhp@yahoo.com>
2020-08-10 20:24:03 -0300

    aula 29

--- examples/curso_flutter/projeto_perguntas/lib/main.dart
+++ examples/curso_flutter/projeto_perguntas/lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  final List<String> perguntas = [
+    'Qual é a sua cor favorita?',
+    'Qual é o seu animal favorito?'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,7 +16,12 @@ class PerguntaApp extends StatelessWidget {
             title: Text('Perguntas'),
           ),
           body: Column(
-            children: [Text('Linha 1'), Text('Linha 2'), Text('Linha 3')],
+            children: [
+              Text(perguntas[0]),
+              RaisedButton(child: Text('Resposta 1'), onPressed: null),
+              RaisedButton(child: Text('Resposta 2'), onPressed: null),
+              RaisedButton(child: Text('Resposta 3'), onPressed: null)
+            ],
           )),
     );
   }
