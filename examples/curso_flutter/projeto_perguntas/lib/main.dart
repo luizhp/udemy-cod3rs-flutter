1ea5f38508a35a4039073808afbd05a371f8a9f4
luizhp <luizhp@yahoo.com>
2020-08-11 17:16:06 -0300

    aula 31

--- examples/curso_flutter/projeto_perguntas/lib/main.dart
+++ examples/curso_flutter/projeto_perguntas/lib/main.dart
@@ -9,7 +9,13 @@ class PerguntaApp extends StatelessWidget {
   ];
 
   void responder() {
-    print('Pergunta respondida');
+    print('Resposta 1 foi selecionada');
+  }
+
+  void Function() funcaoQueRetornaUmaOutraFuncao() {
+    return () {
+      print('Resposta 4 foi selecionada');
+    };
   }
 
   @override
@@ -23,8 +29,18 @@ class PerguntaApp extends StatelessWidget {
             children: [
               Text(perguntas[0]),
               RaisedButton(child: Text('Resposta 1'), onPressed: responder),
-              RaisedButton(child: Text('Resposta 2'), onPressed: responder),
-              RaisedButton(child: Text('Resposta 3'), onPressed: responder)
+              RaisedButton(
+                  child: Text('Resposta 2'),
+                  onPressed: () {
+                    print('Resposta 2 foi selecionada');
+                  }),
+              RaisedButton(
+                  child: Text('Resposta 3'),
+                  onPressed: () => print('Resposta 3 foi selecionada')),
+              RaisedButton(
+                  child: Text('Resposta 4'),
+                  onPressed: funcaoQueRetornaUmaOutraFuncao()),
+              RaisedButton(child: Text('Resposta 5'), onPressed: null)
             ],
           )),
     );
