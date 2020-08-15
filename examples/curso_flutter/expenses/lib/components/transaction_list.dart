d2c683d37ca5f8368f80605e34b9fd8426a2ea75
luizhp <luizhp@yahoo.com>
2020-08-15 16:42:49 -0300

    aula 105 - Adicionar Imagem

--- examples/curso_flutter/expenses/lib/components/transaction_list.dart
+++ examples/curso_flutter/expenses/lib/components/transaction_list.dart
@@ -11,59 +11,77 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final tr = transactions[index];
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    ),
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    NumberFormat.currency(locale: "pt-BR", symbol: "R\$")
-                        .format(tr.value),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
+                SizedBox(height: 20),
+                Text(
+                  'Nenhuma transação cadastrada',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tr.title,
-                        style: Theme.of(context).textTheme.headline6,
-                        // style: TextStyle(
-                        //   fontSize: 16,
-                        //   fontWeight: FontWeight.bold,
-                        // ),
-                      ),
-                      Text(
-                        DateFormat('d MMM y', 'pt-BR').format(tr.date),
-                        style: TextStyle(
-                          color: Colors.grey[600],
-                        ),
-                      )
-                    ])
+                SizedBox(height: 20),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                )
               ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                final tr = transactions[index];
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context).primaryColor,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          NumberFormat.currency(locale: "pt-BR", symbol: "R\$")
+                              .format(tr.value),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tr.title,
+                              style: Theme.of(context).textTheme.headline6,
+                              // style: TextStyle(
+                              //   fontSize: 16,
+                              //   fontWeight: FontWeight.bold,
+                              // ),
+                            ),
+                            Text(
+                              DateFormat('d MMM y', 'pt-BR').format(tr.date),
+                              style: TextStyle(
+                                color: Colors.grey[600],
+                              ),
+                            )
+                          ])
+                    ],
+                  ),
+                );
+              },
+              // children: transactions.map((tr) {}).toList(),
             ),
-          );
-        },
-        // children: transactions.map((tr) {}).toList(),
-      ),
     );
   }
 }
--- examples/curso_flutter/expenses/lib/main.dart
+++ examples/curso_flutter/expenses/lib/main.dart
@@ -49,66 +49,66 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _transactions = <Transaction>[
-    Transaction(
-      id: 't1',
-      title: 'Novo Tênis de Corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't3',
-      title: 'Conta de Água',
-      value: 80.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't4',
-      title: 'Internet',
-      value: 81.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't6',
-      title: 'Supermercado',
-      value: 411.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't5',
-      title: 'IPTU',
-      value: 51.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't7',
-      title: 'Livros',
-      value: 41.89,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't8',
-      title: 'Pedágio',
-      value: 21.0,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't9',
-      title: 'Combustível',
-      value: 110.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't10',
-      title: 'Celular',
-      value: 65.30,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Novo Tênis de Corrida',
+    //   value: 310.76,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Conta de Luz',
+    //   value: 211.30,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't3',
+    //   title: 'Conta de Água',
+    //   value: 80.30,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't4',
+    //   title: 'Internet',
+    //   value: 81.30,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't6',
+    //   title: 'Supermercado',
+    //   value: 411.30,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't5',
+    //   title: 'IPTU',
+    //   value: 51.30,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't7',
+    //   title: 'Livros',
+    //   value: 41.89,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't8',
+    //   title: 'Pedágio',
+    //   value: 21.0,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't9',
+    //   title: 'Combustível',
+    //   value: 110.30,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't10',
+    //   title: 'Celular',
+    //   value: 65.30,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   _addTransaction(String title, double value) {
