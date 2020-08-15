2d1c0b6ec16f6323aeec5c7e0bf94419a397262c
luizhp <luizhp@yahoo.com>
2020-08-15 14:41:14 -0300

    aula 097 - Botões para Adicionar Transação

--- examples/curso_flutter/expenses/lib/main.dart
+++ examples/curso_flutter/expenses/lib/main.dart
@@ -18,24 +18,33 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Despesas Pessoais'),
+      appBar: AppBar(title: Text('Despesas Pessoais'), actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {},
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                  width: double.infinity,
-                  child: Card(
-                    color: Colors.blue,
-                    child: Text('Gráfico'),
-                    elevation: 5,
-                  )),
-              TransactionUser(),
-            ],
-          ),
-        ));
+      ]),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('Gráfico'),
+                  elevation: 5,
+                )),
+            TransactionUser(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
