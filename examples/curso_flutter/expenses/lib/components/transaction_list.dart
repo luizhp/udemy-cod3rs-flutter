ebb79ef3ebdf15c1aaf8fb559dfeecaaf1ff11d4
luizhp <luizhp@yahoo.com>
2020-08-15 14:21:49 -0300

    aula 095 - Usando ListView #1

--- examples/curso_flutter/expenses/lib/components/transaction_list.dart
+++ examples/curso_flutter/expenses/lib/components/transaction_list.dart
@@ -11,56 +11,54 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tr) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 10,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      NumberFormat.currency(locale: "pt-BR", symbol: "R\$")
-                          .format(tr.value),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
+      child: ListView(
+        children: transactions.map((tr) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
                   ),
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tr.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    NumberFormat.currency(locale: "pt-BR", symbol: "R\$")
+                        .format(tr.value),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text(
-                          DateFormat('d MMM y', 'pt-BR').format(tr.date),
-                          style: TextStyle(
-                            color: Colors.grey[600],
-                          ),
-                        )
-                      ])
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                      ),
+                      Text(
+                        DateFormat('d MMM y', 'pt-BR').format(tr.date),
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                        ),
+                      )
+                    ])
+              ],
+            ),
+          );
+        }).toList(),
       ),
     );
   }
