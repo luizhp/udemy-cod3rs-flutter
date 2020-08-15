e81650afb9c2adc306d2a9845937d498a009a3e6
luizhp <luizhp@yahoo.com>
2020-08-15 14:53:11 -0300

    aula 098 - Exibindo Formulário (Modal)

--- examples/curso_flutter/expenses/lib/main.dart
+++ examples/curso_flutter/expenses/lib/main.dart
@@ -1,7 +1,12 @@
-import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+import 'dart:math';
+import 'package:expenses/components/transaction_form.dart';
+import 'package:expenses/components/transaction_list.dart';
+import 'package:expenses/models/transaction.dart';
+import 'components/transaction_form.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -14,14 +19,103 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = <Transaction>[
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Conta de Água',
+      value: 80.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Internet',
+      value: 81.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't6',
+      title: 'Supermercado',
+      value: 411.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'IPTU',
+      value: 51.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't7',
+      title: 'Livros',
+      value: 41.89,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't8',
+      title: 'Pedágio',
+      value: 21.0,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't9',
+      title: 'Combustível',
+      value: 110.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't10',
+      title: 'Celular',
+      value: 65.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return TransactionForm(null);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Despesas Pessoais'), actions: <Widget>[
         IconButton(
           icon: Icon(Icons.add),
-          onPressed: () {},
+          onPressed: () => _openTransactionFormModal(context),
         ),
       ]),
       body: SingleChildScrollView(
@@ -36,13 +130,13 @@ class MyHomePage extends StatelessWidget {
                   child: Text('Gráfico'),
                   elevation: 5,
                 )),
-            TransactionUser(),
+            TransactionList(_transactions),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _openTransactionFormModal(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
