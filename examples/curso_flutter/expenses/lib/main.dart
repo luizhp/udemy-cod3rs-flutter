651a58611465388094d210552a6a9d90f55d224d
luizhp <luizhp@yahoo.com>
2020-08-15 15:02:28 -0300

    aula 101 - Fechando Formulário (Modal)

--- examples/curso_flutter/expenses/lib/main.dart
+++ examples/curso_flutter/expenses/lib/main.dart
@@ -99,6 +99,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
   _openTransactionFormModal(BuildContext context) {
