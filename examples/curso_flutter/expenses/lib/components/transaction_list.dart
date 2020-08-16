d28bfb9f3193af1792c11f97865fb66d37fcdec7
luizhp <luizhp@yahoo.com>
2020-08-15 21:20:17 -0300

    aula 121 - Excluindo Transação

--- examples/curso_flutter/expenses/lib/components/transaction_list.dart
+++ examples/curso_flutter/expenses/lib/components/transaction_list.dart
@@ -4,8 +4,9 @@ import 'package:expenses/models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+  final void Function(String) onRemove;
 
-  TransactionList(this.transactions);
+  TransactionList(this.transactions, this.onRemove);
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +54,11 @@ class TransactionList extends StatelessWidget {
                         style: Theme.of(context).textTheme.headline6),
                     subtitle: Text(
                       DateFormat('d MMM y').format(tr.date),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => onRemove(tr.id),
                     ),
                   ),
                 );
