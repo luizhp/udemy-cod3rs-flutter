eff12b1d1f83b9b515e2b4113569242a44f582d0
luizhp <luizhp@yahoo.com>
2020-08-16 11:58:26 -0300

    aula 138 - Objeto Media Query

--- examples/curso_flutter/expenses/lib/main.dart
+++ examples/curso_flutter/expenses/lib/main.dart
@@ -160,14 +160,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
         title: Text(
           'Despesas Pessoais',
           // style: TextStyle(
-          // fontSize: 20 * MediaQuery.of(context).textScaleFactor,
+          // fontSize: 20 * mediaQuery.textScaleFactor,
           // fontFamily: 'OpenSans',
           // )
         ),
@@ -187,9 +187,9 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ]);
 
-    final availableHeight = MediaQuery.of(context).size.height -
+    final availableHeight = mediaQuery.size.height -
         appBar.preferredSize.height -
-        MediaQuery.of(context).padding.top;
+        mediaQuery.padding.top;
 
     return Scaffold(
       appBar: appBar,
