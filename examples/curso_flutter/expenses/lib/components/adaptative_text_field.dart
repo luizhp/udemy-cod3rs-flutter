7534535e4bd10b1fd8828676a1d953e73e9ed477
luizhp <luizhp@yahoo.com>
2020-08-16 13:12:54 -0300

    aula 144 - Desafio TextField Adaptativo - Resposta

--- examples/curso_flutter/expenses/lib/components/adaptative_text_field.dart
+++ examples/curso_flutter/expenses/lib/components/adaptative_text_field.dart
@@ -1,31 +1,44 @@
-// import 'dart:io';
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AdaptativeTextField extends StatelessWidget {
-  final TextEditingController controller;
-  final Function onSubmitted;
-  final TextInputType keyboardType;
   final String label;
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final Function(String) onSubmitted;
 
   AdaptativeTextField({
-    this.controller,
-    this.onSubmitted,
-    this.keyboardType,
     this.label,
+    this.controller,
+    this.keyboardType = TextInputType.text,
+    this.onSubmitted,
   });
 
   @override
   Widget build(BuildContext context) {
-    // return Platform.isIOS
-    //     ? Container()
-    //     : TextField(
-    return TextField(
-      controller: controller,
-      keyboardType: keyboardType,
-      onSubmitted: onSubmitted,
-      decoration: InputDecoration(
-        labelText: label,
-      ),
-    );
+    return Platform.isIOS
+        ? Padding(
+            padding: const EdgeInsets.only(
+              bottom: 10,
+            ),
+            child: CupertinoTextField(
+                controller: controller,
+                keyboardType: keyboardType,
+                onSubmitted: onSubmitted,
+                placeholder: label,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 6,
+                  vertical: 12,
+                )),
+          )
+        : TextField(
+            controller: controller,
+            keyboardType: keyboardType,
+            onSubmitted: onSubmitted,
+            decoration: InputDecoration(
+              labelText: label,
+            ),
+          );
   }
 }
