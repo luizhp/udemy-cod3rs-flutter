620ac44f3bd3e7e4722fde7d46a5c01a48a247d9
luizhp <luizhp@yahoo.com>
2020-08-16 13:27:32 -0300

    aula 146 - DatePicker Adaptativo

--- examples/curso_flutter/expenses/lib/components/transaction_form.dart
+++ examples/curso_flutter/expenses/lib/components/transaction_form.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:expenses/components/adaptative_button.dart';
 import 'package:expenses/components/adaptative_text_field.dart';
+import 'package:expenses/components/adaptative_date_picker.dart';
 
 class TransactionForm extends StatefulWidget {
   final void Function(String, double, DateTime) onSubmit;
@@ -25,25 +25,6 @@ class _TransactionFormState extends State<TransactionForm> {
       return;
     }
     this.widget.onSubmit(title, value, _selectedDate);
-  }
-
-  _showDatePicker() {
-    showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2019),
-      lastDate: DateTime.now(),
-    ).then((pickedDate) {
-      print('Executado dentro do then!!!');
-      if (pickedDate == null) {
-        return;
-      }
-      setState(() {
-        _selectedDate = pickedDate;
-      });
-    });
-
-    print('Executado!!!');
   }
 
   @override
@@ -73,30 +54,15 @@ class _TransactionFormState extends State<TransactionForm> {
               onSubmitted: (_) => _submitForm(),
               label: 'Valor (R\$)',
             ),
-            Container(
-              height: 70,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      _selectedDate == null
-                          ? 'Nenhuma data selecionada!'
-                          : 'Data Selecionada: ${DateFormat('dd/MM/y').format(_selectedDate)}',
-                    ),
-                  ),
-                  FlatButton(
-                    textColor: Theme.of(context).primaryColor,
-                    child: Text(
-                      'Selecionar Data',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    onPressed: _showDatePicker,
-                  ),
-                ],
-              ),
+            AdaptativeDatePicker(
+              selectedDate: _selectedDate,
+              onDateChanged: (newDate) {
+                setState(() {
+                  _selectedDate = newDate;
+                });
+              },
             ),
+            // Novo Componente Data adaptativo,
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
