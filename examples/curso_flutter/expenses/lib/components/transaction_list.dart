6bf2a890cb22929c494cf89185ffea18740f65e2
luizhp <luizhp@yahoo.com>
2020-08-16 11:55:44 -0300

    aula 137 - Aproveitando Larguras Maiores

--- examples/curso_flutter/expenses/lib/components/transaction_list.dart
+++ examples/curso_flutter/expenses/lib/components/transaction_list.dart
@@ -48,7 +48,7 @@ class TransactionList extends StatelessWidget {
                     child: Padding(
                       padding: const EdgeInsets.all(6),
                       child: FittedBox(
-                        child: Text('R\$${tr.value}'),
+                        child: Text('R\$${tr.value.toStringAsFixed(2)}'),
                       ),
                     ),
                   ),
@@ -57,11 +57,18 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat('d MMM y').format(tr.date),
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () => onRemove(tr.id),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 480
+                      ? FlatButton.icon(
+                          onPressed: () => onRemove(tr.id),
+                          icon: Icon(Icons.delete),
+                          label: Text('Excluir'),
+                          textColor: Theme.of(context).errorColor,
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => onRemove(tr.id),
+                        ),
                 ),
               );
             },
