8aee8d4f177b57d8d1f67037fcca1037a0152802
luizhp <luizhp@yahoo.com>
2020-08-23 10:16:41 -0300

    aula 173 - Passando Dados via Construtor

--- examples/curso_flutter/meals/lib/screens/categories_meals_screen.dart
+++ examples/curso_flutter/meals/lib/screens/categories_meals_screen.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+
+  const CategoriesMealsScreen(this.category);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Receitas',
+          category.title,
         ),
       ),
       body: Center(
         child: Text(
-          'Receitas por Categoria',
+          'Receitas por Categoria ${category.id}',
         ),
       ),
     );
