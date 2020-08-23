5456ac1279fb8a5baae7305ac84ea33dccbe97e2
luizhp <luizhp@yahoo.com>
2020-08-23 14:23:22 -0300

    aula 188 - Substituindo Telas

--- examples/curso_flutter/meals/lib/components/main_drawer.dart
+++ examples/curso_flutter/meals/lib/components/main_drawer.dart
@@ -44,12 +44,13 @@ class MainDrawer extends StatelessWidget {
           _creatItem(
             Icons.restaurant,
             'Refeições',
-            () => Navigator.of(context).pushNamed(AppRoutes.HOME),
+            () => Navigator.of(context).pushReplacementNamed(AppRoutes.HOME),
           ),
           _creatItem(
             Icons.settings,
             'Configurações',
-            () => Navigator.of(context).pushNamed(AppRoutes.SETTINGS),
+            () =>
+                Navigator.of(context).pushReplacementNamed(AppRoutes.SETTINGS),
           ),
         ],
       ),
