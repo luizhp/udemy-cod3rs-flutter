c2f03730872216665948f16669bea96c6e901678
luizhp <luizhp@yahoo.com>
2020-08-23 11:02:53 -0300

    aula 178 - Componente Item Refeição #01

--- examples/curso_flutter/meals/lib/screens/categories_meals_screen.dart
+++ examples/curso_flutter/meals/lib/screens/categories_meals_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 import 'package:meals/data/dummy_data.dart';
 
+import 'package:meals/components/meal_item.dart';
+
 class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -20,7 +22,7 @@ class CategoriesMealsScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: categoryMeals.length,
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(categoryMeals[index]);
         },
       ),
     );
