46a61b7000bab5bb314972b7e6fc6f628aef8ae7
luizhp <luizhp@yahoo.com>
2020-08-26 19:23:25 -0300

    aula 207 - Inherited Widget na Prática - #6 desativação

--- examples/curso_flutter/shop/lib/main.dart
+++ examples/curso_flutter/shop/lib/main.dart
@@ -3,26 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'package:shop/views/products_overview_screen.dart';
 import 'package:shop/views/product_detail_screen.dart';
-import 'package:shop/providers/counter_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
-      child: MaterialApp(
-        title: 'Minha Loja',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato',
-        ),
-        home: ProductsOverviewScreen(),
-        routes: {
-          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
-        },
+    return MaterialApp(
+      title: 'Minha Loja',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
+      home: ProductsOverviewScreen(),
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+      },
     );
   }
 }
--- examples/curso_flutter/shop/lib/views/product_detail_screen.dart
+++ examples/curso_flutter/shop/lib/views/product_detail_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
-import 'package:shop/providers/counter_provider.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   // final Product product;
@@ -18,37 +17,9 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
         ModalRoute.of(context).settings.arguments as Product;
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(product.title),
-        ),
-        body: Column(children: <Widget>[
-          Text(CounterProvider.of(context).state.value.toString()),
-          RaisedButton(
-            child: Text('+'),
-            onPressed: () {
-              setState(() {
-                context
-                    .dependOnInheritedWidgetOfExactType<CounterProvider>()
-                    .state
-                    .inc();
-                // CounterProvider.of(context).state.inc();
-                print(CounterProvider.of(context).state.value);
-              });
-            },
-          ),
-          RaisedButton(
-            child: Text('-'),
-            onPressed: () {
-              setState(() {
-                context
-                    .dependOnInheritedWidgetOfExactType<CounterProvider>()
-                    .state
-                    .dec();
-                // CounterProvider.of(context).state.dec();
-                print(CounterProvider.of(context).state.value);
-              });
-            },
-          ),
-        ]));
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
+    );
   }
 }
