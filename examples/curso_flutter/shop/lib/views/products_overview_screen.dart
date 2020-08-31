58bb5b737bf7863da14598acd4194f2d102b125a
luizhp <luizhp@yahoo.com>
2020-08-31 18:55:37 -0300

    aula 211 - Usando Provider

--- examples/curso_flutter/shop/lib/views/products_overview_screen.dart
+++ examples/curso_flutter/shop/lib/views/products_overview_screen.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
-import 'package:shop/data/dummy_data.dart';
+import 'package:shop/providers/products.dart';
 import 'package:shop/widgets/product_item.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
-  final List<Product> loadedProducts = DUMMY_PRODUCTS;
-
   @override
   Widget build(BuildContext context) {
+    final productsProvider = Provider.of<Products>(context);
+    final products = productsProvider.items;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
       body: GridView.builder(
         padding: const EdgeInsets.all(10),
-        itemCount: this.loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(this.loadedProducts[i]),
+        itemCount: products.length,
+        itemBuilder: (ctx, i) => ProductItem(products[i]),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
