bc4c5734ca8181c9b674dc543003a2aae95a5469
luizhp <luizhp@yahoo.com>
2020-09-06 16:54:28 -0300

    aula 214 - ChangeNotifierProvider.value

--- examples/curso_flutter/shop/lib/widgets/product_grid.dart
+++ examples/curso_flutter/shop/lib/widgets/product_grid.dart
@@ -12,8 +12,9 @@ class ProductGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (ctx) => products[i],
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        // create: (ctx) => products[i],
+        value: products[i],
         child: ProductItem(),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
