a176d58bebdaca83f0affb82a9413ae7b0eb967d
luizhp <luizhp@yahoo.com>
2020-09-07 18:33:55 -0300

    aula 217 - Filtrar Favoritos - #1

--- examples/curso_flutter/shop/lib/providers/products.dart
+++ examples/curso_flutter/shop/lib/providers/products.dart
@@ -5,7 +5,24 @@ import 'package:shop/data/dummy_data.dart';
 class Products with ChangeNotifier {
   List<Product> _items = DUMMY_PRODUCTS;
 
-  List<Product> get items => [..._items];
+  bool _showFavoriteOnly = false;
+
+  List<Product> get items {
+    if (this._showFavoriteOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    }
+    return [..._items];
+  }
+
+  void showFavoriteOnly() {
+    this._showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    this._showFavoriteOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
--- examples/curso_flutter/shop/lib/views/products_overview_screen.dart
+++ examples/curso_flutter/shop/lib/views/products_overview_screen.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/widgets/product_grid.dart';
+import 'package:shop/providers/products.dart';
+
+enum FilterOptions { Favorite, All }
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Products products = Provider.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Minha Loja'),
-      ),
+      appBar: AppBar(title: Text('Minha Loja'), actions: <Widget>[
+        PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorite) {
+                products.showFavoriteOnly();
+              } else if (selectedValue == FilterOptions.All) {
+                products.showAll();
+              }
+              print(selectedValue);
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+                  PopupMenuItem(
+                    child: Text('Somente Favoritos'),
+                    value: FilterOptions.Favorite,
+                  ),
+                  PopupMenuItem(
+                    child: Text('Todos'),
+                    value: FilterOptions.All,
+                  ),
+                ])
+      ]),
       body: ProductGrid(),
     );
   }
